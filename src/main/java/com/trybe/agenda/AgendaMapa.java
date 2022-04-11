ae25f6a84b53b3cf9a1e73fc01ab0ae7e7f6c5df
ricardo-sousa <rwmsousa@gmail.com>
2022-04-11 12:09:20 -0300

    feat: resolution exercise

--- src/main/java/com/trybe/agenda/AgendaMapa.java
+++ src/main/java/com/trybe/agenda/AgendaMapa.java
@@ -1,7 +1,38 @@
 package com.trybe.agenda;
 
+import java.util.HashMap;
+
 public class AgendaMapa {
+  private HashMap<String, String> agenda = new HashMap<String, String>();
 
+  public void incluirContato(String name, String telefone) {
+    agenda.put(name, telefone);
+  }
 
+  public void excluirContato(String name) {
+    agenda.remove(name);
+  }
 
+  public boolean verificaContato(String name) {
+    return agenda.containsKey(name);
+  }
+
+  public int tamanhoAgenda() {
+    return agenda.size();
+  }
+
+  public void atualizarTelefone(String name, String telefone) {
+    agenda.put(name, telefone);
+  }
+
+  public String obterTelefone(String name) {
+    return agenda.get(name);
+  }
+
+  public static void main(String[] args) {
+    AgendaMapa agenda = new AgendaMapa();
+    agenda.incluirContato("João", "88889999");
+    agenda.incluirContato("Maria", "77779999");
+    agenda.verificaContato("João");
+  }
 }
